836db7d55ab63b88102f8af1368755e087d428d7
Jangwon Kim <116819389+jangwonkim-cocel@users.noreply.github.com>
2024-07-16 21:07:33 +0900

    Update base.py

--- co_gym/algorithms/base.py
+++ co_gym/algorithms/base.py
@@ -1,1 +1,35 @@
+from abc import ABC, abstractmethod
+from typing import (TypeVar, Any)
 
+CriticType = TypeVar("CriticType")
+ActorType = TypeVar("ActorType")
+BufferType = TypeVar("BufferType")
+OptimType = TypeVar("OptimType")
+
+
+class BaseAlgorithm(ABC):
+    def __init__(self):
+        self.name: str
+        self.type: str  # "on_policy" or "off_policy"
+        self.policy: Any
+        self.critic: Any
+        self.worker_buffer: Any
+        self.sampler_buffer: Any
+        super().__init__()
+
+    @abstractmethod
+    def train_critic(self, *inputs):
+        pass
+
+    @abstractmethod
+    def train_actor(self, *inputs):
+        pass
+
+    @abstractmethod
+    def train_both(self, *inputs):
+        pass
+
+    @abstractmethod
+    def cal_target(self, buffer: BufferType, critic: CriticType) -> BufferType:
+        pass
+      
