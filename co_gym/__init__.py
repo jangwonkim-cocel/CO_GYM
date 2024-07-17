4137a7fef9f1cb1322ec59e9e0a0ce124b69c131
Jangwon Kim <116819389+jangwonkim-cocel@users.noreply.github.com>
2024-07-17 22:48:18 +0900

    Update __init__.py

--- co_gym/__init__.py
+++ co_gym/__init__.py
@@ -1,5 +1,5 @@
 from co_gym.envs.register import make
 from co_gym.trainer import load
-from co_gym.tester import Tester
+
 __version__ = '0.2.1'
 __author__ = 'jangwonkim@postech.ac.kr, jaehyungcho@postech.ac.kr'
